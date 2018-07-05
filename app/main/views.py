f7206f9b13e3ba0f9d550d254d6a1d0563822a36
Benard18 <benkaranja43@gmail.com>
2018-07-05 10:57:20 +0300

    linking the google chrome with the website

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,17 @@ from ..models import DoctorUser
 def index():
 	title="Doctors"
 	return render_template("index.html",title=title)
+
+@main.route('/newappointment/',methods=["GET","POST"])
+@login_required
+def addappointment():
+	start=request.form['start']
+	end=request.form['end']
+	email='ben.karanja@live.com'
+	# patient=current_user.email
+	currentuser=current_user.username
+
+	sendemail(email,start,end,currentuser)
+
+	title='Doctors'
+	return render_template("index.html",title=title)
