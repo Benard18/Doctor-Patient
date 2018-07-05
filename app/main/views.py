42a6b23e06f223cdcb0c98ca9dd23d2ce24e1a6e
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-07-05 13:32:55 +0300

    push all my work #brighton-asumani

--- app/main/views.py
+++ app/main/views.py
@@ -8,8 +8,3 @@ from . import main
 def index():
 	title="Doctors"
 	return render_template("index.html",title=title)
-
-
-@main.route('/authdoc/login')
-def logindoc():
-	return render_template('login.html')
