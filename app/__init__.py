c5607aa0f8a55ddbb41ba99b8d84087b37a29650
Benard18 <benkaranja43@gmail.com>
2018-07-06 13:18:49 +0300

    removing simple

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
-from flask_simplemde import SimpleMDE
+
 
 
 
@@ -13,7 +13,6 @@ from flask_simplemde import SimpleMDE
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'main.login'
-simple = SimpleMDE()
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
@@ -31,7 +30,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    simple.init_app(app)
+    
 
     # Registering the blueprint
     from .main import main as main_blueprint
