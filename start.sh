802be18748977f1579a56da801902323b1b40e3c
Benard18 <benkaranja43@gmail.com>
2018-07-11 15:54:13 +0300

    added space

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 export DATABASE_URL='postgres://losocwzbmkbcyx:bed87e20f3793d217a8fba69f04bec1bf0c977c8851a9ec3d3e3b63c31a4de4f@ec2-107-22-241-243.compute-1.amazonaws.com:5432/d43t1d3tigjk8j'
-python manage.py server
+python  manage.py server
